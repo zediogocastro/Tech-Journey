4cde6f2b4ca675f0fab23e9020c9007f512d95f1
zediogocastro <83618323+zediogocastro@users.noreply.github.com>
2024-08-05 09:39:17 +0000

    addds small update

--- learn/learn_modules/script.py
+++ learn/learn_modules/script.py
@@ -6,7 +6,7 @@ words_counter = [Counter("abca"), Counter("aaa")]
 summed_counters = text_analyzer.sum_counters(words_counter)
 print(summed_counters)
 
-my_istance = text_analyzer.SocialMedia("test #ai is correct ! test #data #ai")
+my_istance = text_analyzer.n("test #ai is correct ! test #data #ai")
 print(my_istance.word_counts.most_common(2))
 
 print(my_istance.hashtag_counts)
