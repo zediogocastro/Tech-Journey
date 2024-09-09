980d5513bd2e37ddc073c00a43d5e932925c2a9d
zediogocastro <83618323+zediogocastro@users.noreply.github.com>
2024-09-09 16:35:06 +0000

    updates sql_queries

--- learn/learn_DataApp/sql_queries.py
+++ learn/learn_DataApp/sql_queries.py
@@ -1,103 +1,96 @@
-table_names = ("songs", "artists", "time", "users", "songplays")
+"""Module providig sql queries"""
+
+table_names = (
+    "players",
+    "player_rankings",  
+    "matches",
+    "tournaments", 
+    "match_scores", 
+    "betting_odds"
+)
 
 # CREATE TABLES
 
-song_table_create = """
-CREATE TABLE IF NOT EXISTS songs (
-  song_id VARCHAR(64) PRIMARY KEY,
-  title VARCHAR,
-  artist_id VARCHAR,
-  year SMALLINT,
-  duration DOUBLE PRECISION
+PLAYERS_TABLE_CREATE = """
+CREATE TABLE IF NOT EXISTS players (
+  player_id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
 );
 """
 
-artists_table_create = """
-CREATE TABLE IF NOT EXISTS artists (
-  artist_id VARCHAR(64) PRIMARY KEY,
-  name VARCHAR,
-  location VARCHAR,
-  latitude DOUBLE PRECISION,
-  longitude DOUBLE PRECISION
+PLAYER_RANKINGS_TABLE_CREATE= """
+CREATE TABLE IF NOT EXISTS player_rankings (
+  ranking_id SERIAL PRIMARY KEY,
+  player_id INT REFERENCES players(player_id),
+  ranking_date DATE NOT NULL,
+  rank INT NOT NULL,
+  points INT,
+  UNIQUE(player_id, ranking_date),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 """
 
-time_table_create = """
-CREATE TABLE IF NOT EXISTS time (
-  start_time TIMESTAMP WITH TIME ZONE PRIMARY KEY,
-  hour SMALLINT,
-  day SMALLINT,
-  week SMALLINT,
-  month SMALLINT,
-  year SMALLINT,
-  weekday SMALLINT
+MATCHES_TABLE_CREATE = """
+CREATE TABLE IF NOT EXISTS matches (
+  match_id SERIAL PRIMARY KEY,
+  tournament_id INT REFERENCES tournaments(tournament_id) ON DELETE CASCADE,
+  date DATE NOT NULL,
+  round VARCHAR(50),
+  best_of INT,
+  winner_id INT REFERENCES players(player_id) ON DELETE CASCADE,
+  loser_id INT REFERENCES players(player_id) ON DELETE CASCADE,
+  winner_rank INT,
+  loser_rank INT,
+  winner_pts INT,
+  loser_pts INT,
+  winner_sets INT CHECK (winner_sets >= 0),
+  loser_sets INT CHECK (loser_sets >= 0),
+  comments TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 """
 
-user_table_create = """
-CREATE TABLE IF NOT EXISTS users (
-  user_id INTEGER PRIMARY KEY,
-  first_name VARCHAR,
-  last_name VARCHAR,
-  gender CHAR(1),
-  level CHAR(4)
-);
-"""
-# gender = "M" or "F"
-# level = "free" or "paid"
-
-songplay_table_create = """
-CREATE TABLE IF NOT EXISTS songplays (
-  songplay_id SERIAL PRIMARY KEY,
-  start_time TIMESTAMP WITH TIME ZONE NOT NULL,
-  user_id INTEGER NOT NULL,
-  level CHAR(4),
-  song_id VARCHAR(64),
-  artist_id VARCHAR(64),
-  session_id INTEGER,
-  location VARCHAR,
-  user_agent VARCHAR
+TOURNAMENTS_TABLE_CREATE= """
+CREATE TABLE IF NOT EXISTS tournaments (
+  tournament_id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  location VARCHAR(100),
+  surface VARCHAR(50),
+  series VARCHAR(50),
+  court VARCHAR(50)
 );
 """
 
-# Insert Records
-SONG_TABLE_INSERT = """
-INSERT INTO songs (song_id, title, artist_id, year, duration)
-VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT (song_id) DO NOTHING
+MATCH_SCORES_TABLE_CREATE = """
+CREATE TABLE IF NOT EXISTS match_scores (
+  score_id SERIAL PRIMARY KEY,
+  match_id INT REFERENCES matches(match_id) ON DELETE CASCADE,
+  set_number INT CHECK (set_number > 0),
+  winner_score INT,
+  loser_score INT
+);
 """
 
-ARTIST_TABLE_INSERT = """
-INSERT INTO artists (artist_id, name, location, latitude, longitude)
-VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT (artist_id) DO NOTHING;
-"""
-
-TIME_TABLE_INSERT = """
-INSERT INTO time (start_time, hour, day, week, month, year, weekday)
-VALUES (%s, %s, %s, %s, %s, %s, %s)
-ON CONFLICT (start_time) DO NOTHING;
-"""
-
-USER_TABLE_INSERT = """
-INSERT INTO users (user_id, first_name, last_name, gender, level)
-VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level
-"""
-
-SONGPLAY_TABLE_INSERT = """
-INSERT INTO songplays (start_time, song_id, artist_id, user_id, level,
-                       session_id, location, user_agent)
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+BETTING_ODDS_TABLE_CREATE = """
+CREATE TABLE IF NOT EXISTS betting_odds (
+  odds_id SERIAL PRIMARY KEY,
+  match_id INT REFERENCES matches(match_id) ON DELETE CASCADE,
+  bookmaker VARCHAR(50),
+  winner_odds DECIMAL(5, 2),
+  loser_odds DECIMAL(5, 2)
+);
 """
 
 # QUERY LISTS
-
 CREATE_TABLE_QUERIES = [
-    song_table_create,
-    artists_table_create,
-    time_table_create,
-    user_table_create,
-    songplay_table_create,
+    PLAYERS_TABLE_CREATE,
+    PLAYER_RANKINGS_TABLE_CREATE,
+    MATCHES_TABLE_CREATE,
+    TOURNAMENTS_TABLE_CREATE,
+    MATCH_SCORES_TABLE_CREATE,
+    BETTING_ODDS_TABLE_CREATE
 ]
+
 DROP_TABLE_QUERIES = [f"DROP TABLE IF EXISTS {table}" for table in table_names]
--- learn/learn_DataApp/test.py
+++
@@ -1,5 +0,0 @@
-from etl import get_files
-
-filepath = "./data/song_data"
-test_data = get_files(filepath, pattern= '*.json')
-print(test_data)
